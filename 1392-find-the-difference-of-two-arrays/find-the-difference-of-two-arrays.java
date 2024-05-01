9646578d8755d1ee6c176dd57309ef1f36ace752
prernapahwa <60768072+prernapahwa@users.noreply.github.com>
2024-05-02 00:19:14 +0530

    Time: 9 ms (95.24%) | Memory: 45.7 MB (9.37%) - LeetSync

--- 1392-find-the-difference-of-two-arrays/find-the-difference-of-two-arrays.java
+++ 1392-find-the-difference-of-two-arrays/find-the-difference-of-two-arrays.java
@@ -2,21 +2,21 @@ class Solution {
     public List<List<Integer>> findDifference(int[] nums1, int[] nums2) {
         List<Integer> ans1 = new ArrayList<Integer>();
         List<Integer> ans2 = new ArrayList<Integer>();
-        HashMap<Integer, Integer> map = new HashMap<>();
-        HashMap<Integer, Integer> map2 = new HashMap<>();
+        HashSet<Integer> map = new HashSet<>();
+        HashSet<Integer> map2 = new HashSet<>();
         for (int value : nums1) {
-            map.put(value, map.getOrDefault(value, 0) + 1); 
+            map.add(value); 
         }
         for (int value : nums2) {
-            map2.put(value, map.getOrDefault(value, 0) + 1);  
+            map2.add(value);  
         }
-        for (Integer key : map.keySet()){
-           if (!map2.containsKey(key)){
+        for (Integer key : map){
+           if (!map2.contains(key)){
              ans1.add(key);
            }
         }
-        for (Integer key : map2.keySet()){
-           if (!map.containsKey(key)){
+        for (Integer key : map2){
+           if (!map.contains(key)){
                 ans2.add(key);
            }
         }
